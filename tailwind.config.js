5ff33d279cf62809977f058fb96b30b9c4942260
David Lee <Dai180293@gmail.com>
2023-07-30 14:48:16 +0100

    added all style variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,18 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
-            fontFamily: {},
-            colors: {},
+            fontFamily: {
+                publicSans: ["Public Sans", "sans-serif"],
+            },
+            colors: {
+                white: "hsl(0, 0%, 100%)",
+                lightGray: "hsl(0, 0%, 98%)",
+                lightGrayBlue: "hsl(220, 16%, 96%)",
+                grayBlue: "hsl(233, 8%, 62%)",
+                cyan: "hsl(192, 70%, 51%)",
+                green: "hsl(136, 65%, 51%)",
+                darkBlue: "hsl(233, 26%, 24%)",
+            },
         },
     },
     // eslint-disable-next-line no-undef
