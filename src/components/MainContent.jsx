164c53a59e4235287d5a51e21551c49d610a6f62
David Lee <Dai180293@gmail.com>
2023-08-02 10:05:11 +0100

    articles section completed

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,37 +1,8 @@
 import { Card, Image, Text, Button, Group } from "@mantine/core";
 import phoneMockup from "../assets/images/image-mockups.png";
-import onlineBanking from "../assets/images/icon-online.svg";
-import budget from "../assets/images/icon-budgeting.svg";
-import onboarding from "../assets/images/icon-onboarding.svg";
-import api from "../assets/images/icon-api.svg";
+import { cards } from "./sectionContent";
+import { articles } from "./sectionContent";
 const MainContent = () => {
-    const cards = [
-        {
-            img: onlineBanking,
-            title: "Online Banking",
-            content:
-                "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world",
-        },
-        {
-            img: budget,
-            title: "Simple Budgeting",
-            content:
-                "See exactly where your money goes each month. Recieve notifications when you're close to hitting limits.",
-        },
-        {
-            img: onboarding,
-            title: "Fast Onboarding",
-            content:
-                "We don't do branches. Open your account in minutes online and start taking control of your finances right away.",
-        },
-        {
-            img: api,
-            title: "Open API",
-            content:
-                "Manage your savings, investments, pension, and much more from one account. Tracking your money has never beene easier.",
-        },
-    ];
-
     const cardContent = cards.map((cardItem, index) => (
         <Card
             className="bg-transparent max-w-[325px] xl:max-w-[280px] min-h-[310px] mx-auto text-center my-10 xl:mx-0 xl:text-left"
@@ -48,6 +19,32 @@ const MainContent = () => {
             <Text className="text-grayBlue">
                 <p>{cardItem.content}</p>
             </Text>
+        </Card>
+    ));
+
+    const articleContent = articles.map((articleItem, index) => (
+        <Card
+            withBorder
+            className="max-w-[325px] xl:max-w-[280px] h-[28rem] mx-auto text-center my-10 xl:mx-0 xl:text-left rounded-lg"
+            key={index}
+        >
+            <Card.Section>
+                <Image src={articleItem.img} alt={articleItem.title} fit="cover" height={200} />
+            </Card.Section>
+
+            <Card.Section className="p-4">
+                <Text className="text-grayBlue">
+                    <sub>By {articleItem.author}</sub>
+                </Text>
+
+                <Text className="py-3 text-xl font-medium">
+                    <h2 className="gradientText">{articleItem.title}</h2>
+                </Text>
+
+                <Text className="text-grayBlue">
+                    <p>{articleItem.content}</p>
+                </Text>
+            </Card.Section>
         </Card>
     ));
 
@@ -83,7 +80,17 @@ const MainContent = () => {
                         Control your finances like never before
                     </p>
                 </article>
-                <Group className="w-full" position="apart">{cardContent}</Group>
+                <Group className="w-full" position="apart">
+                    {cardContent}
+                </Group>
+            </section>
+            <section className="flex flex-col justify-evenly items-start min-h-[700px] bg-white">
+                <h2 className="text-3xl lg:text-4xl mb-5 w-full mt-12 text-center xl:text-left">
+                    Latest Articles
+                </h2>
+                <Group className="w-full" position="apart">
+                    {articleContent}
+                </Group>
             </section>
         </main>
     );
---
+++ src/components/sectionContent.js
@@ -0,0 +1,66 @@
+import onlineBanking from "../assets/images/icon-online.svg";
+import budget from "../assets/images/icon-budgeting.svg";
+import onboarding from "../assets/images/icon-onboarding.svg";
+import api from "../assets/images/icon-api.svg";
+import article1 from "../assets/images/image-currency.jpg";
+import article2 from "../assets/images/image-restaurant.jpg";
+import article3 from "../assets/images/image-plane.jpg";
+import article4 from "../assets/images/image-confetti.jpg";
+
+export const cards = [
+    {
+        img: onlineBanking,
+        title: "Online Banking",
+        content:
+            "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world",
+    },
+    {
+        img: budget,
+        title: "Simple Budgeting",
+        content:
+            "See exactly where your money goes each month. Recieve notifications when you're close to hitting limits.",
+    },
+    {
+        img: onboarding,
+        title: "Fast Onboarding",
+        content:
+            "We don't do branches. Open your account in minutes online and start taking control of your finances right away.",
+    },
+    {
+        img: api,
+        title: "Open API",
+        content:
+            "Manage your savings, investments, pension, and much more from one account. Tracking your money has never beene easier.",
+    },
+];
+
+export const articles = [
+    {
+        img: article1,
+        author: "Claire Robinson",
+        title: "Recieve money un any currency with no fees",
+        content:
+            "The world is getting smaller and we're becoming more mobile. So why should you be forced to only recieve money in a single ...",
+    },
+    {
+        img: article2,
+        author: "Wilson Hutton",
+        title: "Treat yourself without worrying about money",
+        content:
+            "Our simple budgeting feature allows yotu to seperate out your spending and set realistic limits each month. That means you ...",
+    },
+    {
+        img: article3,
+        author: "Wilson Hutton",
+        title: "Take your Easybank card wherever you go",
+        content:
+            "We want you to enjoy your travels. This is why we don't charge any fees on purchases while you're abroad. We'll even show you ...",
+    },
+    {
+        img: article4,
+        author: "Claire Robinson",
+        title: "Our invite-only Beta accounts are no live",
+        content:
+            "After a lot of hard work by the whole team, we're excited to launch our closed beta. It's easy to request an invite through the site ...",
+    },
+];
